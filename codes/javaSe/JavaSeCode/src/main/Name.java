708840906df9eca17feb843be273f151fc1bdcd5
NarminAbdullazada <abdullazadanarmin1@gmail.com>
2024-09-13 18:37:01 +0400

    variable

---
+++ codes/javaSe/JavaSeCode/src/main/Name.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Name {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("age: " + age);
+
+        boolean hasHome = true;
+        System.out.println("hasHome: " + hasHome);
+
+        char studentGrade = 'A';
+        System.out.println("studentGrade: " + studentGrade);
+
+        double productPrice = 3.8;
+        System.out.println("productPrice: " + productPrice);
+
+        String studentName = "Yaqub";
+        System.out.println("studentName: " + studentName);
+    }
+}
+
+
